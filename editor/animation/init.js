28f5d79bdd3bd3eca34035615c671de515cbbba2
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-01-05 12:42:19 +1200

    animation prepare

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -77,13 +77,9 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
             //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
+            var canvas = new MatrixPattern($content.find(".explanation")[0]);
+
+            canvas.prepare(checkioInput[0], checkioInput[1]);
 
 
             this_e.setAnimationHeight($content.height() + 60);
@@ -106,27 +102,95 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 //            });
 //        });
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+        function MatrixPatternCanvas(root) {
+            var colorOrange4 = "#F0801A";
+            var colorOrange3 = "#FA8F00";
+            var colorOrange2 = "#FAA600";
+            var colorOrange1 = "#FABA00";
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+            var colorBlue4 = "#294270";
+            var colorBlue3 = "#006CA9";
+            var colorBlue2 = "#65A1CF";
+            var colorBlue1 = "#8FC7ED";
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#9D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+            var colorGrey4 = "#737370";
+            var colorGrey3 = "#9D9E9E";
+            var colorGrey2 = "#C5C6C6";
+            var colorGrey1 = "#EBEDED";
 
-        var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+            var colorWhite = "#FFFFFF";
 
+            var maxSizeX = 380;
+            var cellSize = 40;
+            var sizeX,
+                sizeY;
+
+            var paper;
+            var cellSet;
+            var numbSet;
+
+            var attrRect0 = {"stroke": colorGrey4, "stroke-width": 2, "fill": colorBlue1};
+            var attrRect1 = {"stroke": colorGrey4, "stroke-width": 2, "fill": colorBlue3};
+            var attrRect2 = {"stroke": colorGrey4, "stroke-width": 2, "fill": colorOrange1};
+            var attrRect3 = {"stroke": colorGrey4, "stroke-width": 2, "fill": colorOrange2};
+            var attrNumb;
+
+            this.prepare = function (pattern, matrix) {
+                var pattern_lx = pattern[0].length;
+                var pattern_ly = pattern.length;
+                var matrix_lx = matrix[0].length;
+                var matrix_ly = matrix.length;
+
+                cellSize = Math.min(cellSize, maxSizeX / matrix_lx);
+                sizeX = cellSize * matrix_lx;
+                sizeY = cellSize * (matrix_ly + pattern_ly + 1);
+                var px0 = (sizeX - cellSize * pattern_lx) / 2;
+                var my0 = cellSize * (pattern_ly + 1);
+
+
+                attrNumb = {"stroke": colorBlue4, "font-size": cellSize * 0.8, "font-family": "Verdana", "font-weight": "bold"};
+                paper = Raphael(root, sizeX, sizeY);
+                cellSet = paper.set();
+                numbSet = paper.set();
+
+                for (var i = 0; i < pattern_ly; i++) {
+                    for (var j = 0; j < pattern_lx; j++) {
+                        var cell = paper.set();
+                        cell.push(
+                            paper.rect(
+                                px0 + j * cellSize,
+                                i * cellSize,
+                                cellSize, cellSize
+                            ).attr(pattern[i][j] === 1 ? attrRect1 : attrRect0),
+                            paper.text(
+                                px0 + (j + 0.5) * cellSize,
+                                (i + 0.5) * cellSize
+                            ).attr(attrNumb)
+                        );
+                    }
+                }
+                for (i = 0; i < matrix_ly; i++) {
+                    for (j = 0; j < matrix_lx; j++) {
+                        cell = paper.set();
+                        cell.push(
+                            paper.rect(
+                                j * cellSize,
+                                my0 + i * cellSize,
+                                cellSize, cellSize
+                            ).attr(pattern[i][j] === 1 ? attrRect1 : attrRect0),
+                            paper.text(
+                                (j + 0.5) * cellSize,
+                                my0 + (i + 0.5) * cellSize
+                            ).attr(attrNumb)
+                        );
+                        cellSet.push(cell);
+                    }
+                }
+
+
+            }
+        }
 
     }
-);
+)
+;
